6441bcf7c4ed68080ed9e9f5cd56511f3f801896
rafaelalbia <rafaelalbia@outlook.com>
2021-05-04 08:01:31 -0300

    Ending 'Welcome' page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,71 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 import wateringImage from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export function Welcome() {
   return (
-    <View>
-      <Text>Gerencie suas plantas de forma fácil</Text>
-      <Image source={wateringImage} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
+      </Text>
+      
+      <Image source={wateringImage} style={styles.image} />
+
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. 
+        Nós cuidamos de lembrar você sempre que precisar.
+      </Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>
+          >
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38
+  },
+  subtitle:{
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 20,
+    color: colors.heading
+  },
+  button:{
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
+  },
+  image: {
+    height: 284,
+    width: 292
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
+  }
+})
